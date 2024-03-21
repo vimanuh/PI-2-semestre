24311edf85d57fc1ab6c259dd4ff0da96c56f2e7
veman <veman@DESKTOP-SP66VRI>
2024-03-21 13:10:55 -0300

    att 21/03

---
+++ PI/src/model/Animal.java
@@ -0,0 +1,70 @@
+
+package model;
+
+public class Animal {
+    
+    private String nome;
+    private String raca;
+    private String detalhes;
+    private int sit;
+    private int id;
+    private String especie;
+    private String cor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    }
+
+    public int getSit() {
+        return sit;
+    }
+
+    public void setSit(int sit) {
+        this.sit = sit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    
+}
---
+++ PI/src/model/User.java
@@ -0,0 +1,44 @@
+
+package model;
+
+
+public class User {
+    
+    private String email;
+    private String pass;
+    private String telefone;
+    private int id;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
